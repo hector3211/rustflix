fc4c5d5efae6e701fef2ba169e95c3301a0a13ed
hector3211e <hectororopesa5@gmail.com>
2023-10-27 19:10:00 -0400

    changed: vercel postgres

--- db/index.ts
+++ db/index.ts
@@ -1,27 +1,15 @@
-// import { createClient } from "@libsql/client";
-// import { migrate } from "drizzle-orm/libsql/migrator";
 import * as schema from "./schema";
-// import { sql } from '@vercel/postgres';
-// import { drizzle } from 'drizzle-orm/vercel-postgres';
-import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
-import postgres from 'postgres';
+import { sql } from "@vercel/postgres";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+// import { drizzle, PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import postgres from "postgres";
 
 // FOR POSTGRES
-const queryClient = postgres(process.env.DB_URL as string);
-export const db = drizzle(queryClient, { schema });
-
-// FOR LIBSQL
-// const client = createClient({
-//     url: process.env.DB_URL as string,
-//     authToken: process.env.DB_AUTH as string,
-// });
-
-// FOR LIBSQL
-// export const db = drizzle(client, { schema });
-
+// const queryClient = postgres(process.env.DB_URL as string);
+// export const db = drizzle(queryClient, { schema });
 
 //FOR VERCEL POSTGRES
-// export const db = drizzle(sql)
+export const db = drizzle(sql, { schema });
 
 // await migrate(db, { migrationsFolder: "drizzle" });
